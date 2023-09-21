0f5d61573245b9a5e5df8425a0d179d81c60d9ec
Suganth Sai  K S <suganthsai.ks@gavstech.com>
2023-09-21 12:04:27 +0530

    First Web Api Commit

---
+++ ASPWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+/*        [HttpGet("/add")]  //if you add '/' it becomes root
+       // [HttpGet]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }*/
+
+        [HttpGet("calculator/add")]  //if you add '/' it becomes root
+                           // [HttpGet]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        // api/calculator/subract? x=10 & y=5
+        [HttpGet("calculator/sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y+1000;
+        }
+        // api/calculator/multiply? x=10 & y=10
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/calculator/add? x=10 & y=2
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ ASPWebApi/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("demo/add")] 
+        // [HttpGet]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
